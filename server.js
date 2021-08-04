7b5c92183b9660c598637a26b8b167b356bcca45
Amalia <amaliaroye@gmail.com>
2021-08-04 13:05:51 -0400

    Create backend

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const chalk = require('chalk')
+
+//- require route files
+const userRoutes = require('./app/routes/user_routes')
+
+// require middleware
+const errorHandler = require('./lib/error_handler')
+const requestLogger = require('./lib/request_logger')
+
+// require database configuration logic
+// `db` will be the actual Mongo URI as a string
+const db = require('./config/db')
+
+// require configured passport authentication middleware
+const auth = require('./lib/auth')
+
+// define ports used for cors and local port declaration
+const serverDevPort = 4741
+const clientDevPort = 7165
+
+// establish database connection
+// use new version of URL parser
+// use createIndex instead of deprecated ensureIndex
+mongoose.connect(db, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+})
+
+const connection = mongoose.connection
+connection.once('open', () => {
+	console.log(
+		chalk.green('MongoDB database connection estabilished successfully!')
+	)
+})
+
+// instantiate express application object
+const app = express()
+
+// set CORS headers on response from this API using the `cors` NPM package
+// `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+app.use(
+	cors({
+		origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
+	})
+)
+
+// define port for API to run on
+const port = process.env.PORT || serverDevPort
+
+// register passport authentication middleware
+app.use(auth)
+
+// add `express.json` middleware which will parse JSON requests into JS objects before they reach the route files.
+// The method `.use` sets up middleware for the Express application
+app.use(express.json())
+// this parses requests sent by `$.ajax`, which use a different content type
+app.use(express.urlencoded({ extended: true }))
+
+// log each request as it comes in for debugging
+app.use(requestLogger)
+
+//- register route files
+app.use(userRoutes)
+
+// register error handling middleware
+// note that this comes after the route middlewares, because it needs to be passed any error messages from them
+app.use(errorHandler)
+
+app.listen(port, () => {
+	console.log(chalk.cyan(`Server is running on port: ${port}`))
+})
+
+// needed for testing
+module.exports = app
